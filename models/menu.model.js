29828fffc0c4fea01fc11a62c512f93a4771be7d
nguyenthai98 <dtruongxuan1397@gmail.com>
2021-04-03 16:03:12 +0700

    update menu

--- models/menu.model.js
+++ models/menu.model.js
@@ -3,13 +3,13 @@ const TBL_PRODUCT = 'product';
 const TBL_DEVICE = 'device';
 
 module.exports = {
-    loadMenu: function (id, table) {
+    loadMenu: function (id_menu, name_product, table) {
         if (table == TBL_PRODUCT) {
-            return db.load(`select p.name_product,p.id_product from menu m JOIN category c on m.id_menu = c.id_menu join product p on c.id_category = p.id_category WHERE m.id_menu = ${id}`);
+            return db.load(`select DISTINCT p.name_product from menu m JOIN category c on m.id_menu = c.id_menu join product p on c.id_category = p.id_category WHERE m.id_menu = ${id_menu}`);
         } else if (table == TBL_DEVICE) {
-            return db.load(`select * from ${TBL_DEVICE} d where ( d.id_product = ${id} and d.isLive = 1 and d.status = 1) or (d.id_product = ${id} and d.isLive = 1 and d.status = 0) or
-            (d.id_product = ${id} and d.isLive = 2 and d.status = 0) or (d.id_product = ${id} and d.isLive = 2 and d.status = 1)
-            `);
+            return db.load(`SELECT * FROM device d WHERE 
+            (d.id_product in (SELECT id_product from product p WHERE p.name_product LIKE '%${id_menu}%') and d.isLive = 1 and d.status = 1) or (d.id_product in (SELECT id_product from product p WHERE p.name_product LIKE '%${id_menu}%') and d.isLive = 1 and d.status = 0) or
+            (d.id_product in (SELECT id_product from product p WHERE p.name_product LIKE '%${id_menu}%') and d.isLive = 2 and d.status = 0) or (d.id_product in (SELECT id_product from product p WHERE p.name_product LIKE '%${id_menu}%') and d.isLive = 2 and d.status = 1)`);
         }
     }
-}  
+}
